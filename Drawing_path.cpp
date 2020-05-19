cc45eb92c71de44bf235f13acac392437054734a
rlenrey <rlenrey@rlenrey.home>
2020-05-19 12:27:39 +0200

    added radius

--- Drawing_path.cpp
+++ Drawing_path.cpp
@@ -10,7 +10,8 @@ void drawing_path(int size_x, int size_y, std::string name)
     sf::Image logo;
     logo.loadFromFile(name);
 
-    int length = rand()%size_x;
+    int x = rand()%10; //i have no idea how to call this 'radius'?
+    int length = rand()%(size_x*5);
     int more_random = (rand() % 8) + 1;
     int direction = (((rand() % 8) + 1) + more_random)/2;
     int pixel_x = (size_x / 2), pixel_y = (size_y / 2);
@@ -21,51 +22,27 @@ void drawing_path(int size_x, int size_y, std::string name)
         switch (direction)
         {
             case 1: // up
-                pixel_y++;
-                pixel_y++;
+                pixel_y += x;
                 break;
 
             case 2: // down
-                pixel_y--;
-                pixel_y--;
+                pixel_y -= x;
                 break;
 
             case 3: // left
-                pixel_x--;
-                pixel_x--;
+                pixel_x -= x;
                 break;
 
             case 4: // right
-                pixel_x--;
-                pixel_x--;
+                pixel_x += x;
                 break;
 
-            case 5: // up, left
-                pixel_y++;
-                pixel_x--;
-                break;
-
-            case 6: // up, right
-                pixel_y++;
-                pixel_x++;
-                break;
-
-            case 7: // down, left
-                pixel_y--;
-                pixel_x--;
-                break;
-
-            case 8: // down, right
-                pixel_y--;
-                pixel_x++;
-                break;
         }
 
+     for (int i = 0; i < x; i++)
+        for (int j = 0; j < x; j++)
+            logo.setPixel(pixel_x + i, pixel_y + j, sf::Color::Green); // This part paints 2x2 pixel "block"
 
-        logo.setPixel(pixel_x, pixel_y, sf::Color::Green); // This part paints 2x2 pixel "block"
-        logo.setPixel(pixel_x + 1, pixel_y, sf::Color::Green);
-        logo.setPixel(pixel_x, pixel_y + 1, sf::Color::Green);
-        logo.setPixel(pixel_x + 1, pixel_y + 1, sf::Color::Green);
 
         direction = (rand() % 8) + 1;
     }
