28b0b41c8b26005798d9c22d9c1eafaf115aea15
rlenrey <rlenrey@rlenrey.home>
2020-06-21 00:29:47 +0200

    tested displaying images

--- Path_maker/graphical/graphical.cpp
+++ Path_maker/graphical/graphical.cpp
@@ -3,10 +3,20 @@
 
 void graphical()
 {
-    sf::Window window(sf::VideoMode(900, 800), "Path_Maker", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode(900, 800), "Path_Maker", sf::Style::Default);
 
     window.setActive(true);
     bool running = true;
+
+     sf::Image image;
+     if (!(image.loadFromFile("Generated/Path_00.png")))
+             std::cout << "Cannot load image";   //Load Image
+
+     sf::Texture texture;
+     texture.loadFromImage(image);  //Load Texture from image
+
+     sf::Sprite sprite;
+     sprite.setTexture(texture);
 
     while (running)
     {
@@ -19,5 +29,9 @@ void graphical()
             if (event.type == sf::Event::Closed)
                 running = false;
         }
+
+               window.clear();
+               window.draw(sprite);
+               window.display();
     }
 }
